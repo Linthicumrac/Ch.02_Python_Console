40d0c175594fd2a8d7e5eee463e6e5b8d908a878
linthicumrac <linthicumrac@urbandaleschools.com>
2022-09-09 08:59:58 -0500

    Python Practice log Commit

---
+++ Python Practice Log.py
@@ -0,0 +1,36 @@
+# This is my learning about python
+# This is another line
+# command / , uncomment comments
+print(10.1 // 2)  # // means floor division - rounds down
+print(10 % 2)  # % or the remainder
+print(abs(-7))  # absolute or make positive
+a = 2
+b = 7
+print(a)
+print("a")  # printing that letter a not 2; differs because it's in parenthesis
+print(b ** a)  # ** means to the ___ power
+print()  # () prints a blank line
+print(b % 2)
+print()
+a, b = (5, "Mr. Teacher")  # assign multiple variables numbers/ words
+print(a)
+print(b)
+print()
+x = 8
+print(x)
+x = x + 1  # reassigning the variable from 8 to 9
+print(x)
+x += 1  # x+=1 means assign x to increase one number ; x+=1 is the same as x=x+1
+bulletsShot = 0
+bulletsShot=bulletsShot+3
+print(bulletsShot)
+print()
+a = "this is some text"
+b = 3
+c = 4.3
+d = True
+print(type(a))  # type of variable ; class str
+print(type(b))  # class integer
+print(type(c))  # class float (decimal/fraction)
+print(type(d))  # class boolean
+
